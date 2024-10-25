13bb1ac7c26a566035292d7fc74d4b656f41d3d3
Vanthu204 <lythu2k4lc@gmail.com>
2024-10-25 12:54:27 +0700

    Các màn hình có trong dự án(Sprint2)

--- app/src/main/java/dunghtph30405/example/nhom6_md18401/frg/frg_phuKien.java
+++
@@ -1,4 +0,0 @@
-package dunghtph30405.example.nhom6_md18401.frg;
-
-public class frg_phuKien {
-}
--- app/src/main/java/dunghtph30405/example/nhom6_md18401/inteface/TotalUpdateListener.java
+++
@@ -1,5 +0,0 @@
-package dunghtph30405.example.nhom6_md18401.inteface;
-
-public interface TotalUpdateListener {
-    void onTotalUpdated(double total);
-}
